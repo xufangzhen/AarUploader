746254ce3f683354d3e740b4f77fa8586b4703ec
xufangzhen <xu_fangzhen@126.com>
2019-09-07 02:40:41 +0800

    [源码依赖]插件中设置源码依赖

--- upload/src/main/groovy/com/xfz/upload/AarUploadPlugin.groovy
+++ upload/src/main/groovy/com/xfz/upload/AarUploadPlugin.groovy
@@ -2,12 +2,18 @@ package com.xfz.upload
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
 
 class AarUploadPlugin implements Plugin<Project> {
     void apply(Project target) {
         target.pluginManager.apply 'maven'
         def aarUploadTask = target.task('aarUpload', type: AarUploadTask, dependsOn: 'assemble')
-        //获取配置的archives
+        aarUploadTask.doFirst {
+            target.task(type: Jar) {
+                from android.sourceSets.main.java.srcDirs
+                classifier = 'sources'
+            }
+        }
         aarUploadTask.configuration = target.configurations.archives
     }
 }
